993ed103fdb23c13023915f7d9e3b0e7d961765d
musasumer <musasumer2020@gmail.com>
2020-06-20 22:43:21 +0300

    conflict kalmadi

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -10,7 +10,7 @@ public class FirstClass {
 
 		
 
-		System.out.println("conflict yapmak istiyoruz");
+		System.out.println("conflict yaptik duzelttik");
 
 		
 		System.out.println("first commit from remote repository");
