c250b4df9297a94b379552d4bac63af945aa4411
musasumer <musasumer2020@gmail.com>
2020-06-20 21:35:09 +0300

    second commit from local repo

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,9 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("first commit from local repostory");
+		System.out.println("first commit from local repository");
+		
+		System.out.println("second commit from local repository");
 
 	}
 
